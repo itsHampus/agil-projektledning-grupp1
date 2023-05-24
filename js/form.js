dc14bb0153d7bc29043e5eca2539b4f0fdc49fe7
Mohammad Jafari <Mohammad.jafari@gritacademy.se>
2023-05-24 10:55:26 +0200

    fixat ansökan formuläret

--- js/form.js
+++ js/form.js
@@ -9,3 +9,42 @@ form.addEventListener('submit', (event) => {
         window.location.href = 'index.html';
     }, 2000);
 });
+
+function validateForm() {
+   
+    var fullName = document.getElementById('full-name').value;
+    var phoneNumber = document.getElementById('phone-number').value;
+    var email = document.getElementById('email').value;
+   
+    if (fullName === '') {
+      alert('Please enter your full name.');
+      return false;
+    }
+  
+    if (phoneNumber === '') {
+      alert('Please enter your phone number.');
+      return false;
+    }
+  
+   
+    var phoneRegex = /^\+46\d{9}$/;
+    if (!phoneRegex.test(phoneNumber)) {
+      alert('Please enter a valid phone number in the format +46xxxxxxxxx.');
+      return false;
+    }
+  
+    if (email === '') {
+      alert('Please enter your email address.');
+      return false;
+    }
+  
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert('Please enter a valid email address.');
+      return false;
+    }
+  
+    return true;
+  }
+  
+  
